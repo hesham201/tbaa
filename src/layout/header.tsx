82d6e6840857bfc416c78f9da31ca91c005d6fd3
Hesham <107617341+hesham201@users.noreply.github.com>
2025-05-31 23:16:56 +0500

    edwef

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -156,7 +156,7 @@ const Header = () => {
       {
         yPercent: -200,
         duration: 1,
-        ease: "power1.out",
+        ease: "power1.in",
         opacity: 1,
       },
       "<+0.2"
