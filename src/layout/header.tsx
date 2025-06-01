0d9cf18227331781de54af3596107f36b94390db
Hesham <107617341+hesham201@users.noreply.github.com>
2025-06-02 04:51:51 +0500

    changes

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -460,7 +460,7 @@ const Header = () => {
 
       <div
         id="menu"
-        className={`fixed top-full bg-[url(/menu-bg.webp)] bg-cover left-0 w-full h-full z-[1000] ${
+        className={`fixed top-full bg-[url(/menu-bg.webp)] bg-cover left-0 w-full h-[100vh] z-[1000] ${
           openMenu ? "" : ""
         }`}
       >
