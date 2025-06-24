b1328c1305b3faadf8afb95ac22c9073ac650876
Hesham <107617341+hesham201@users.noreply.github.com>
2025-06-24 13:56:07 +0500

    changes

--- src/views/testimonial/testimonial-main.tsx
+++ src/views/testimonial/testimonial-main.tsx
@@ -1,74 +1,96 @@
-import React from "react";
+"use client";
+import React, { useLayoutEffect, useRef } from "react";
 import Container from "@/components/container";
 import { TESTIMONIALS } from "@/constant/data";
 import Image from "next/image";
+// import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+
+// gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollTrigger);
+
 const TestimonialMain = () => {
+  const mainRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    const timeout = setTimeout(() => {
+      const items = gsap.utils.toArray<HTMLDivElement>(".testimonial-item");
+      if (!items) return;
+      // let counter = 1;
+
+      items.forEach((item, index) => {
+        gsap.to(item, {
+          opacity: index === items.length - 1 ? 1 : 0,
+          scale: 0.9,
+          transformOrigin: "center center",
+          scrollTrigger: {
+            trigger: item,
+            start: "top 35%",
+            scroller: "[data-scroll-container]", // 👈 IMPORTANT
+            end: index === items.length - 1 ? "bottom 22%" : "bottom 22%",
+            pin: true,
+            pinSpacing: false,
+            scrub: true,
+            // markers: true,
+            // onEnter: () => {
+            //   counter++;
+            //   if (index !== items.length - 1) {
+            //     ScrollTrigger.refresh();
+            //   }
+            //   console.log("Entered:", counter);
+            // },
+          },
+        });
+      });
+    }, 500);
+    return () => clearTimeout(timeout);
+  });
+
   return (
-    <div className="py-10">
+    <div ref={mainRef} className="py-10">
       <Container>
-        <div className="grid gap-10">
+        <div className="relative grid grid-cols-1 gap-10">
           {TESTIMONIALS.map((item) => (
-            <div
-              key={item.name}
-              className={`py-7 px-10 rounded-4xl flex flex-row items-center gap-16 ${
-                item.reverse ? "flex-row-reverse" : ""
-              }`}
-              style={{
-                background: "linear-gradient(to right,#CFAE91,#031A37)",
-              }}
-            >
-              <div className="text-white grow">
-                <div className="flex gap-1 mb-3 flex-row">
-                  <Image
-                    src={"/star.svg"}
-                    alt="stars"
-                    width={100}
-                    height={100}
-                    className="w-[30px] h-[30px]"
-                  />
-                  <Image
-                    src={"/star.svg"}
-                    alt="stars"
-                    width={100}
-                    height={100}
-                    className="w-[30px] h-[30px]"
-                  />
-                  <Image
-                    src={"/star.svg"}
-                    alt="stars"
-                    width={100}
-                    height={100}
-                    className="w-[30px] h-[30px]"
-                  />
-                  <Image
-                    src={"/star.svg"}
-                    alt="stars"
-                    width={100}
-                    height={100}
-                    className="w-[30px] h-[30px]"
-                  />
-                  <Image
-                    src={"/star.svg"}
-                    alt="stars"
-                    width={100}
-                    height={100}
-                    className="w-[30px] h-[30px]"
+            <div key={item.name} className="relative  overflow-hidden">
+              <div
+                className={`testimonial-item py-7 px-10 rounded-4xl flex flex-row items-center gap-16 ${
+                  item.reverse ? "flex-row-reverse" : ""
+                }`}
+                style={{
+                  background: "linear-gradient(to right,#CFAE91,#031A37)",
+                }}
+              >
+                <div className="text-white grow">
+                  <div className="flex gap-1 mb-3 flex-row">
+                    {Array(5)
+                      .fill(0)
+                      .map((_, i) => (
+                        <Image
+                          key={i}
+                          src={"/star.svg"}
+                          alt="stars"
+                          width={100}
+                          height={100}
+                          className="w-[30px] h-[30px]"
+                        />
+                      ))}
+                  </div>
+                  <h2 className="text-3xl mb-4">{item.name}</h2>
+                  <p
+                    className="text-lg"
+                    dangerouslySetInnerHTML={{ __html: item.para }}
                   />
                 </div>
-                <h2 className="text-3xl mb-4">{item.name}</h2>
-                <p
-                  className="text-lg"
-                  dangerouslySetInnerHTML={{ __html: item.para }}
-                />
-              </div>
-              <div className="w-[200px] shrink-0 h-[200px]">
-                <Image
-                  src={"/profile.jpg"}
-                  width={300}
-                  className="w-full h-full object-cover rounded-full"
-                  height={300}
-                  alt={item.name}
-                />
+                <div className="w-[200px] shrink-0 h-[200px]">
+                  <Image
+                    src={"/profile.jpg"}
+                    width={300}
+                    height={300}
+                    className="w-full h-full object-cover rounded-full"
+                    alt={item.name}
+                  />
+                </div>
               </div>
             </div>
           ))}
