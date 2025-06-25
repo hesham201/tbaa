45eb3ac83ada578a74d622e6f8bc0c9f120f26ac
Hesham <107617341+hesham201@users.noreply.github.com>
2025-06-25 17:26:30 +0500

    changes

--- src/views/home/our-events.tsx
+++ src/views/home/our-events.tsx
@@ -1,66 +1,109 @@
 "use client";
 
-import { useRef, useLayoutEffect } from "react";
+import { useRef } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
 
 import Button from "@/components/button";
 import Container from "@/components/container";
 import HeadingTwo from "@/components/heading-two";
-
-gsap.registerPlugin(ScrollTrigger);
+import { useGSAP } from "@gsap/react";
+// gsap.registerPlugin(ScrollTrigger);
 
 const OurEvents = () => {
   const bgWrapperRef = useRef<HTMLDivElement>(null);
   const bgImageRef = useRef<HTMLImageElement>(null);
-
-  useLayoutEffect(() => {
-    if (!bgWrapperRef.current || !bgImageRef.current) return;
-    const timeout = setTimeout(() => {
-      gsap.fromTo(
-        bgImageRef.current,
-        {
-          y: "-10%", // Start position (slightly above)
-          scale: 1.2,
-        },
-        {
-          y: "10%", // End position (slightly below)
-          scale: 1,
-          ease: "none",
-          scrollTrigger: {
-            trigger: bgWrapperRef.current,
-            scroller: "[data-scroll-container]", // Remove if not using Locomotive Scroll
-            start: "top bottom",
-            end: "bottom top",
-            scrub: true,
-          },
-        }
-      );
-      gsap.from("#our-events-heading", {
-        y: 50,
-        opacity: 1,
-        duration: 0.3,
+  useGSAP(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.fromTo(
+      bgImageRef.current,
+      {
+        y: "-10%", // Start position (slightly above)
+        scale: 1.2,
+      },
+      {
+        y: "10%", // End position (slightly below)
+        scale: 1,
+        ease: "none",
         scrollTrigger: {
-          trigger: "#our-events-heading",
+          trigger: bgWrapperRef.current,
           scroller: "[data-scroll-container]", // Remove if not using Locomotive Scroll
-          start: "top 76%",
+          start: "top bottom",
           end: "bottom top",
+          scrub: true,
         },
-      });
-      gsap.from("#buttons-events ", {
-        y: 10,
-        opacity: 1,
-        duration: 0.5,
-        scrollTrigger: {
-          trigger: "#buttons-events",
-          scroller: "[data-scroll-container]", // Remove if not using Locomotive Scroll
-          start: "top 80%",
-          end: "bottom top",
-        },
-      });
-    }, 300);
-    return () => clearTimeout(timeout);
-  }, []);
+      }
+    );
+    gsap.from("#our-events-heading", {
+      y: 50,
+      opacity: 1,
+      duration: 0.3,
+      scrollTrigger: {
+        trigger: "#our-events-heading",
+        scroller: "[data-scroll-container]", // Remove if not using Locomotive Scroll
+        start: "top 76%",
+        end: "bottom top",
+      },
+    });
+    gsap.from("#buttons-events ", {
+      y: 10,
+      opacity: 1,
+      duration: 0.5,
+      scrollTrigger: {
+        trigger: "#buttons-events",
+        scroller: "[data-scroll-container]", // Remove if not using Locomotive Scroll
+        start: "top 80%",
+        end: "bottom top",
+      },
+    });
+  });
+  // useLayoutEffect(() => {
+  //   if (!bgWrapperRef.current || !bgImageRef.current) return;
+  //   const timeout = setTimeout(() => {
+  //     gsap.fromTo(
+  //       bgImageRef.current,
+  //       {
+  //         y: "-10%", // Start position (slightly above)
+  //         scale: 1.2,
+  //       },
+  //       {
+  //         y: "10%", // End position (slightly below)
+  //         scale: 1,
+  //         ease: "none",
+  //         scrollTrigger: {
+  //           trigger: bgWrapperRef.current,
+  //           scroller: "[data-scroll-container]", // Remove if not using Locomotive Scroll
+  //           start: "top bottom",
+  //           end: "bottom top",
+  //           scrub: true,
+  //         },
+  //       }
+  //     );
+  //     gsap.from("#our-events-heading", {
+  //       y: 50,
+  //       opacity: 1,
+  //       duration: 0.3,
+  //       scrollTrigger: {
+  //         trigger: "#our-events-heading",
+  //         scroller: "[data-scroll-container]", // Remove if not using Locomotive Scroll
+  //         start: "top 76%",
+  //         end: "bottom top",
+  //       },
+  //     });
+  //     gsap.from("#buttons-events ", {
+  //       y: 10,
+  //       opacity: 1,
+  //       duration: 0.5,
+  //       scrollTrigger: {
+  //         trigger: "#buttons-events",
+  //         scroller: "[data-scroll-container]", // Remove if not using Locomotive Scroll
+  //         start: "top 80%",
+  //         end: "bottom top",
+  //       },
+  //     });
+  //   }, 300);
+  //   return () => clearTimeout(timeout);
+  // }, []);
 
   return (
     <div ref={bgWrapperRef} className="py-5 lg:py-10 relative overflow-hidden">
