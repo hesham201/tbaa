c43684eed04b7e38eb2130660cf3b9f198076bfc
Hesham <107617341+hesham201@users.noreply.github.com>
2025-06-25 15:54:26 +0500

    changes

--- src/views/presidents/main.tsx
+++ src/views/presidents/main.tsx
@@ -1,7 +1,7 @@
 import Banner from "@/components/banner";
 import { PAST_PRESIDENTS_BANNER } from "@/constant/data";
 import React from "react";
-import Content from "./content";
+// import Content from "./content";
 import ExecutiveTeam from "./executive-team";
 import Faqs from "@/components/faqs";
 
@@ -9,7 +9,7 @@ const Main = () => {
   return (
     <>
       <Banner data={PAST_PRESIDENTS_BANNER} />
-      <Content />
+      {/* <Content /> */}
       <ExecutiveTeam />
       <Faqs />
     </>
