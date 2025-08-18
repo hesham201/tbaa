d682905867488941e5d8568abf5b6c3cd1e76970
Programmer <107617341+hesham201@users.noreply.github.com>
2025-08-18 18:16:54 +0500

    changes

--- src/views/contact-us/banner.tsx
+++ src/views/contact-us/banner.tsx
@@ -67,7 +67,7 @@ const Banner = () => {
       <div className=" upper w-full bg-midnight origin-bottom scale-y-0 ">
         <Container>
           <div className="py-16 ">
-            <h1 className="text-primary text-center mb-3 text-6xl" id="home-h1">
+            <h1 className="text-primary text-center text-6xl" id="home-h1">
               Contact Us
             </h1>
           </div>
