3387ce83f6828846fd8d7e5a1bb6db1fb83ce75f
Programmer <107617341+hesham201@users.noreply.github.com>
2025-08-28 11:50:11 +0500

    changes

--- src/views/contact-us/contact-form.tsx
+++ src/views/contact-us/contact-form.tsx
@@ -3,59 +3,71 @@
 import { useEffect, useRef, useState } from "react";
 import lottie from "lottie-web";
 import Container from "@/components/container";
-import { useFormik } from "formik";
+import { Formik, Field, Form, ErrorMessage, FormikHelpers } from "formik";
 import * as Yup from "yup";
 import emailjs from "emailjs-com";
 
 const ContactForm = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const [submissionMessage, setSubmissionMessage] = useState("");
+  const [submission, setSubmition] = useState(
+    "Thank you for taking the time to complete the contact form. A member of our team will be in touch with you soon. We look forward to helping you achieve your best smile."
+  );
+  const [privacyChecked, setPrivacyChecked] = useState(false);
 
   // Define validation schema with Yup
   const validationSchema = Yup.object({
     name: Yup.string().required("Please enter your name"),
-    email: Yup.string().email("Invalid email format").required("Please enter your email"),
+    email: Yup.string()
+      .email("Invalid email format")
+      .required("Please enter your email"),
     message: Yup.string().required("Please enter your message"),
-    acceptPolicy: Yup.boolean().oneOf([true], "You must accept the Privacy Policy")
+    privacyPolicy: Yup.boolean().oneOf(
+      [true],
+      "You must accept the Privacy Policy"
+    ),
   });
-  
-  // Initialize Formik
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      message: "",
-      acceptPolicy: false
+  const sendEmail = (
+    values: {
+      name: string;
+      email: string;
+      message: string;
+      privacyPolicy: boolean;
     },
-    validationSchema,
-    validateOnBlur: true, // Validate when field loses focus
-    validateOnChange: false, // Don't validate on every keystroke
-    onSubmit: (values, { resetForm }) => {
-      setIsLoading(true);
-      emailjs
-        .sendForm(
-          "service_2cnqd96",
-          "template_ur2kjim",
-          "#contactForm",
-          "nQBJzdhm_0rQ4QbLv"
-        )
-        .then(
-          () => {
-            setIsLoading(false);
-            setIsSubmitted(true);
-            setSubmissionMessage("Form submitted successfully!");
-            resetForm();
-          },
-          () => {
-            setIsLoading(false);
-            setIsSubmitted(true);
-            setSubmissionMessage("Failed to send message, please try again.");
-          }
-        );
-    }
-  });
-  
+    formikHelpers: FormikHelpers<{
+      name: string;
+      email: string;
+      message: string;
+      privacyPolicy: boolean;
+    }>
+  ) => {
+    setIsLoading(true);
+    emailjs
+      .sendForm(
+        "service_2cnqd96",
+        "template_ur2kjim",
+        "#contactForm",
+        "nQBJzdhm_0rQ4QbLv"
+      )
+      .then(
+        () => {
+          console.log(values);
+          setIsLoading(false);
+          setIsSubmitted(true);
+          // alert("Message sent successfully!");
+          formikHelpers.resetForm(); // Reset the form after successful submission
+          // setPrivacyChecked(false); // Reset custom checkbox
+        },
+        () => {
+          // console.log(error.text);
+          console.log(values);
+          setSubmition("Failed to send message, please try again.");
+          setIsSubmitted(true);
+          setIsLoading(false);
+        }
+      );
+  };
+
   const animRefs = [
     useRef<HTMLDivElement>(null),
     useRef<HTMLDivElement>(null),
@@ -116,97 +128,152 @@ const ContactForm = () => {
           {/* Right side (form) */}
           <div className="bg-midnight py-10 px-4 shadow-2xl">
             <h3 className="text-4xl mb-4 text-white">Get In Touch</h3>
-            <form id="contactForm" onSubmit={formik.handleSubmit}>
-              <div className="flex flex-col gap-4">
-                {/* Name */}
-                <div>
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={formik.values.name}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    placeholder="Enter your name"
-                    className="border-white placeholder:text-white text-white border bg-transparent h-[43px] p-4 w-full focus:outline-none"
-                  />
-                  {formik.touched.name && formik.errors.name && (
-                    <p className="text-red-500 text-sm mt-1">*{formik.errors.name}</p>
-                  )}
-                </div>
+            <Formik
+              initialValues={{
+                name: "",
+                email: "",
+                message: "",
+                privacyPolicy: false,
+              }}
+              validationSchema={validationSchema}
+              onSubmit={sendEmail}
+            >
+              {({ setFieldValue }) => (
+                <Form id="contactForm">
+                  <div className="flex flex-col gap-4">
+                    {/* Name */}
+                    <div>
+                      <Field
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Enter your name"
+                        className="border-white placeholder:text-white text-white border bg-transparent h-[43px] p-4 w-full focus:outline-none"
+                      />
+                      <ErrorMessage
+                        name="name"
+                        component="p"
+                        className="text-[#b30000] font-bold mt-2 text-sm"
+                      />
+                    </div>
 
-                {/* Email */}
-                <div>
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    value={formik.values.email}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    placeholder="Enter your email address"
-                    className="border-white placeholder:text-white text-white border bg-transparent h-[43px] p-4 w-full focus:outline-none"
-                  />
-                  {formik.touched.email && formik.errors.email && (
-                    <p className="text-red-500 text-sm mt-1">*{formik.errors.email}</p>
-                  )}
-                </div>
+                    {/* Email */}
+                    <div>
+                      <Field
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Enter your email address"
+                        className="border-white placeholder:text-white text-white border bg-transparent h-[43px] p-4 w-full focus:outline-none"
+                      />
+                      <ErrorMessage
+                        name="email"
+                        component="p"
+                        className="text-[#b30000] font-bold mt-2 text-sm"
+                      />
+                    </div>
 
-                {/* Message */}
-                <div>
-                  <textarea
-                    id="message"
-                    name="message"
-                    value={formik.values.message}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    placeholder="Enter your message"
-                    className="placeholder:text-white p-4 w-full h-[120px] bg-transparent border border-white text-white focus:outline-none"
-                  ></textarea>
-                  {formik.touched.message && formik.errors.message && (
-                    <p className="text-red-500 text-sm mt-1">*{formik.errors.message}</p>
-                  )}
-                </div>
-                
-                {/* Privacy Policy Checkbox */}
-                <div className="">
-                  <label className="flex items-center text-white">
-                    <input
-                      type="checkbox"
-                      id="acceptPolicy"
-                      name="acceptPolicy"
-                      checked={formik.values.acceptPolicy}
-                      onChange={formik.handleChange}
-                      onBlur={formik.handleBlur}
-                      className="mr-2"
+                    {/* Message */}
+                    <div>
+                      <Field
+                        as="textarea"
+                        id="message"
+                        name="message"
+                        placeholder="Enter your message"
+                        className="placeholder:text-white p-4 w-full h-[120px] bg-transparent border border-white text-white focus:outline-none"
+                      ></Field>
+                      <ErrorMessage
+                        name="message"
+                        component="p"
+                        className="text-[#b30000] font-bold mt-2 text-sm"
+                      />
+                    </div>
+
+                    {/* Privacy Policy Checkbox */}
+                    <div className="">
+                      <label className="flex items-center text-white">
+                        <Field
+                          type="checkbox"
+                          checked={privacyChecked}
+                          className="h-[22px] w-[22px]"
+                          name="privacyPolicy"
+                          onChange={(
+                            e: React.ChangeEvent<HTMLInputElement>
+                          ) => {
+                            setPrivacyChecked(e.target.checked); // Update custom checkbox state
+                            setFieldValue("privacyPolicy", e.target.checked); // Update Formik's field value
+                          }}
+                        />
+                        <span>
+                          Please tick this box to agree to our{" "}
+                          <a
+                            href="/privacy-policy"
+                            className="text-primary  hover:text-white"
+                          >
+                            {" "}
+                            Privacy Policy
+                          </a>{" "}
+                          before continuing.
+                        </span>
+                      </label>
+                    </div>
+                    <ErrorMessage
+                      name="privacyPolicy"
+                      component="p"
+                      className="text-[#b30000] font-bold mt-2 text-sm"
                     />
-                    <span>Please tick this box to agree to our <a href="/privacy-policy" className="text-primary  hover:text-white"> Privacy Policy</a> before continuing.</span>
-                  </label>
-                  {formik.touched.acceptPolicy && formik.errors.acceptPolicy && (
-                    <p className="text-red-500 text-sm mt-1">*{formik.errors.acceptPolicy}</p>
-                  )}
-                </div>
-              </div>
+                  </div>
 
-              <div className="mt-3">
-                <button
-                  type="submit"
-                  disabled={isLoading}
-                  className="text-xl uppercase cursor-pointer text-white bg-primary px-7 py-2"
-                >
-                  {isLoading ? "Sending..." : "Send Message"}
-                </button>
-              </div>
-              
-              {isSubmitted && (
-                <div className={`mt-4 p-3 rounded ${submissionMessage.includes("success") ? "bg-green-100 text-green-800" : "bg-red-100 text-red-800"}`}>
-                  {submissionMessage}
-                </div>
+                  <div className="mt-3">
+                    <button
+                      type="submit"
+                      disabled={isLoading}
+                      className="text-xl uppercase cursor-pointer text-white bg-primary px-7 py-2"
+                    >
+                      {isLoading ? "Sending..." : "Send Message"}
+                    </button>
+                  </div>
+                </Form>
               )}
-            </form>
+            </Formik>
           </div>
         </div>
       </Container>
+      {isSubmitted && (
+        <div
+          className="fixed inset-0 backdrop-blur bg-black/50 flex items-center justify-center z-[10003]"
+          onClick={() => setIsSubmitted(false)}
+        >
+          <div
+            className={` relative flex-col lg:flex-row justify-between items-center bg-cover px-9 py-10 rounded-tr-3xl rounded-bl-3xl max-w-[800px] bg-primary`}
+          >
+            <div className="w-full">
+              <p className="text-white text-center text-xl ">{submission}</p>
+            </div>
+            <div className="w-full lg:w-[40%]">
+              <button
+                className="absolute cursor-pointer top-5 right-5 text-white text-2xl"
+                onClick={() => setIsSubmitted(false)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
